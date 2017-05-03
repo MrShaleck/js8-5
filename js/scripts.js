4d0ff949aa75136421d70a75ce5bf6e4b79c1375
Marcin <marcinrszalek@gmail.com>
2017-05-03 15:10:23 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var points = 1
+console.log( 'bie¿¹ca wartoœæ zmiennej points to: ', points ); // 1
+console.log( 'bie¿¹ca wartoœæ zmiennej points to: ', ++points ); // 1
+console.log( 'bie¿¹ca wartoœæ zmiennej points to: ', points ); // 2
