f1edd8a734f016aaaaf8d5fbd5b0d3661156e414
MrShaleck <marcinrszalek@gmail.com>
2017-05-07 14:31:08 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 var a = 3,
 	h = 5,
 	triangleArea = a*h/2;
-console.log('Pole tr?jk?ta o podstawie a: ' + a + ' i wysoko?ci h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
